c74792018051ec00963354a635dadd62173e2664
김정완 <krwd1117@gmail.com>
2024-12-02 11:33:59 +0900

    프로젝트 생성

---
+++ TiccleCollector/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  TiccleCollector
+//
+//  Created by Jeongwan Kim on 12/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .systemBackground
+        title = "Ticcle Collector"
+        
+        // 추가적인 UI 구성은 여기에 구현하시면 됩니다
+    }
+}
